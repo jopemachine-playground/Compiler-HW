c2c2b62fcee2427d98abaf08dd0ffe105fe45761
jopebot <jopemachine@naver.com>
2019-10-08 22:48:53 +0900

    Update Hw03

--- src/cnu/compiler19/hw3/MiniCPrintListener.java
+++ src/cnu/compiler19/hw3/MiniCPrintListener.java
@@ -12,14 +12,6 @@ public class MiniCPrintListener extends MiniCBaseListener {
     }
 
     @Override
-    public void exitExpr_stmt(MiniCParser.Expr_stmtContext ctx) {
-
-        String s = newTexts.get(ctx);
-        newTexts.put(ctx, s);
-
-    }
-
-    @Override
     public void enterExpr(MiniCParser.ExprContext ctx) {
         //System.out.println("enterExpr: " + ctx.getText());
 
@@ -34,7 +26,6 @@ public class MiniCPrintListener extends MiniCBaseListener {
             newTexts.put(ctx.getChild(2), ctx.getChild(2).getText());
         }
     }
-
 
     @Override
     public void exitExpr(MiniCParser.ExprContext ctx) {
@@ -54,18 +45,73 @@ public class MiniCPrintListener extends MiniCBaseListener {
             newTexts.put(ctx, s1 + " " + op + " " + s2);
         }
     }
+    @Override
+    public void exitExpr_stmt(MiniCParser.Expr_stmtContext ctx) {
+
+//        String a1 = ctx.getChild(0).getText();
+//        String a2 = ctx.getChild(1).getText();
+//
+//        String s = ctx.getChild(0).getText();
+
+//        String ss = ctx.getText();
+        newTexts.put(ctx, newTexts.get(ctx.getChild(0)) + ";");
+    }
 
 
     @Override
     public void exitStmt(MiniCParser.StmtContext ctx) {
-        System.out.println(ctx.getText());
-        System.out.println(newTexts.get(ctx));
+//        System.out.println(ctx.getText());
+
+        // System.out.println(newTexts.get(ctx.getChild(0)));
+    }
+
+    @Override
+    public void exitCompound_stmt(MiniCParser.Compound_stmtContext ctx) {
+//        System.out.println(ctx.getChildCount());
+        String a1 = ctx.getChild(0).getText();
+        String a2 = ctx.getChild(1).getText();
+        String a3 = ctx.getChild(2).getText();
+        String s = ctx.getText();
+
+        String[] content = newTexts.get(ctx.getChild(1).getChild(0)).split("\n");
+        StringBuilder builder = new StringBuilder();
+        for (String str : content){
+            builder.append("...." + str + "\n");
+        }
+
+        newTexts.put(ctx, "{\n" + builder.toString() + "}");
+    }
+
+    @Override
+    public void exitIf_stmt(MiniCParser.If_stmtContext ctx) {
+        String s  = ctx.getText();
+        String a1 = ctx.getChild(0).getText();
+        String a2 = ctx.getChild(1).getText();
+        String a3 = ctx.getChild(2).getText();
+        String a4 = ctx.getChild(3).getText();
+        String a5 = ctx.getChild(4).getText();
+
+        newTexts.put(ctx, "if (" + newTexts.get(ctx.getChild(2)) + ")\n" + newTexts.get(ctx.getChild(4).getChild(0)));
+    }
+
+    @Override
+    public void exitFun_decl(MiniCParser.Fun_declContext ctx) {
+        String s  = ctx.getText();
+        String a1 = ctx.getChild(0).getText();
+        String a2 = ctx.getChild(1).getText();
+        String a3 = ctx.getChild(2).getText();
+        String a4 = ctx.getChild(3).getText();
+        String a5 = ctx.getChild(4).getText();
+        String a6 = ctx.getChild(5).getText();
+
+        newTexts.put(ctx, ctx.getChild(0).getText() + " " + ctx.getChild(1).getText() + "(" + ctx.getChild(3).getText() + ")\n" + newTexts.get(ctx.getChild(5)));
+
     }
 
     @Override
     public void exitProgram(MiniCParser.ProgramContext ctx) {
-        System.out.println(ctx.getText());
-        System.out.println(newTexts.get(ctx));
+
+        System.out.println(newTexts.get(ctx.getChild(0).getChild(0)));
 
     }
 }
