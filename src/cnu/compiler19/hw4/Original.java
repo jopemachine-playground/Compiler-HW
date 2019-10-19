dfbbdb436e2a7a6202fa099ffb4c8316875d4311
jopebot <jopemachine@naver.com>
2019-10-19 16:25:06 +0900

    Add Hw04

--- src/cnu/compiler19/hw4/Original.java
+++
@@ -1,5 +0,0 @@
-package cnu.compiler19.hw4;
-
-public class Original {
-
-}
---
+++ src/cnu/compiler19/hw4/Test.java
@@ -0,0 +1,19 @@
+package cnu.compiler19.hw4;
+
+public class Test {
+
+    public Test(){
+
+    }
+
+    public static int add(int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+    public static void main(String[] args){
+        int k = 33;
+        System.out.println(add(1, k));
+    }
+
+}
